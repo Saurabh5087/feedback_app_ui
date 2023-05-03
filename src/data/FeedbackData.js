6c5f1f3587f6362afd6ff54fcf9118386f02f8ad
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-03 10:46:26 +0530

    FeedbackList Component and Global App State

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eveniet unde recusandae odit beatae accusamus praesentium, omnis minima fuga temporibus.'
+    },
+    {
+        id: 2,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eveniet unde recusandae odit beatae accusamus praesentium, omnis minima fuga temporibus.'
+    },
+    {
+        id: 3,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eveniet unde recusandae odit beatae accusamus praesentium, omnis minima fuga temporibus.'
+    }
+]
+
+export default FeedbackData
